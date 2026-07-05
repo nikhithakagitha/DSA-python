efa0aab77ed92387c4eec2a5289c02a48eeeca6a
nikhithakagitha <sainikhithakagitha@gmail.com>
2026-07-05 12:19:30 +0530

    Update hello.py

--- Basics/hello.py
+++ Basics/hello.py
@@ -1,1 +1,17 @@
-print("My DSA journey started")
+1_merging sorted arrays:
+n=int(input())
+t=list(map(int,input().split()))
+m=int(input())
+p=list(map(int,input().split()))
+i=0
+j=0
+while(i<n and j<m):
+     if(t[i]<p[j]):
+        print((t[i]),end="")
+        i+=1
+     else:
+        print((p[j]),end="")
+        j=j+1
+
+    
+        
